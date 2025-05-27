a242a0a058fb2914c454edc36c2b31c24326bf3b
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2025-05-27 09:27:29 -0400

    Combo batch mode now needs extra flags also

--- rebuild_all_leaderboards.sh
+++ rebuild_all_leaderboards.sh
@@ -7,22 +7,23 @@ if [ -z "$YEARS" ]; then
 fi
 if [ "$YEARS" = "all" ] || [ "$YEARS" = "new" ]; then
   npm run build_leaderboards -- --tier=High $ENRICH $EXTRA; npm run build_leaderboards -- --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --gender=Women $ENRICH $EXTRA; 
-   npm run build_leaderboards -- --tier=Combo
+   npm run build_leaderboards -- --tier=Combo $EXTRA
 fi
 if [ "$YEARS" = "all" ] || [ "$YEARS" = "old" ]; then
    npm run build_leaderboards -- --year=2023/24 --tier=High $ENRICH $EXTRA; npm run build_leaderboards -- --year=2023/24 --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --year=2023/24 --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --year=2023/24 --gender=Women --tier=High $ENRICH $EXTRA;
-   npm run build_leaderboards -- --tier=Combo --year=2023/24
+   npm run build_leaderboards -- --tier=Combo --year=2023/24 $EXTRA
    npm run build_leaderboards -- --year=2022/23 --tier=High $ENRICH $EXTRA; npm run build_leaderboards -- --year=2022/23 --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --year=2022/23 --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --year=2022/23 --gender=Women --tier=High $ENRICH $EXTRA;
-   npm run build_leaderboards -- --tier=Combo --year=2022/23
+   npm run build_leaderboards -- --tier=Combo --year=2022/23 $EXTRA
    npm run build_leaderboards -- --year=2021/22 --tier=High $ENRICH $EXTRA; npm run build_leaderboards -- --year=2021/22 --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --year=2021/22 --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --year=2021/22 --gender=Women --tier=High $ENRICH $EXTRA;
-   npm run build_leaderboards -- --tier=Combo --year=2021/22
+   npm run build_leaderboards -- --tier=Combo --year=2021/22 $EXTRA
    npm run build_leaderboards -- --year=2020/21 --tier=High $ENRICH $EXTRA; npm run build_leaderboards -- --year=2020/21 --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --year=2020/21 --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --year=2020/21 --gender=Women --tier=High $ENRICH $EXTRA;
-   npm run build_leaderboards -- --tier=Combo --year=2020/21
+   npm run build_leaderboards -- --tier=Combo --year=2020/21 $EXTRA
+   # Older data with only one tier:
    npm run build_leaderboards -- --tier=High --year=2019/20 $ENRICH $EXTRA; npm run build_leaderboards -- --tier=High --year=2018/9 $ENRICH $EXTRA;
    npm run build_leaderboards -- --gender=Women --year=2019/20 $ENRICH $EXTRA; npm run build_leaderboards -- --gender=Women --year=2018/9 $ENRICH $EXTRA; 
    npm run build_leaderboards -- --tier=High --year=Extra $ENRICH $EXTRA; 
 fi
 if [[ $YEARS =~ ^[0-9]{4}[/][0-9]{2}$ ]]; then
    npm run build_leaderboards -- --year=$YEARS --tier=High $ENRICH $EXTRA;npm run build_leaderboards -- --year=$YEARS --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --year=$YEARS --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --year=$YEARS --gender=Women --tier=High $ENRICH $EXTRA;
-   npm run build_leaderboards -- --tier=Combo --year=$YEARS
+   npm run build_leaderboards -- --tier=Combo --year=$YEARS $EXTRA
 fi
