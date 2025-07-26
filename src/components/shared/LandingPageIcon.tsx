3fb0daf1ae419a369d062d69ac2821d7bd6f95a5
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2025-07-25 23:25:55 -0400

    Attempt to detect chrome dark mode

--- src/components/shared/LandingPageIcon.tsx
+++ src/components/shared/LandingPageIcon.tsx
@@ -1,15 +1,38 @@
 // React imports:
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { UrlRouting } from "../../utils/UrlRouting";
 
 const LandingPageIcon: React.FunctionComponent<{}> = () => {
+  const [darkMode, setDarkMode] = useState(
+    _.thru(undefined, (__) => {
+      // We can also use JavaScript to generate the detection element.
+      const detectionDiv = document.createElement("div");
+      detectionDiv.style.display = "none";
+      detectionDiv.style.backgroundColor = "canvas";
+      detectionDiv.style.colorScheme = "light";
+      document.body.appendChild(detectionDiv);
+      // If the computed style is not white then the page is in Auto Dark Theme.
+      const isAutoDark =
+        getComputedStyle(detectionDiv).backgroundColor != "rgb(255, 255, 255)";
+
+      // remove the detection element from the DOM.
+      document.body.removeChild(detectionDiv);
+      return isAutoDark;
+    })
+  );
   return (
     <a
       href={UrlRouting.getLandingPageUrl({})}
       className="float-left"
       style={{ position: "relative", top: "2px" }}
     >
-      <img src="images/Large_Banner.jpg" />
+      <img
+        src={
+          darkMode
+            ? "images/Large_Banner_invert.jpg"
+            : "images/Large_Banner.jpg"
+        }
+      />
     </a>
   );
 };
