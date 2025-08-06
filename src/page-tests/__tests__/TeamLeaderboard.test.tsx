108cb8334b549da0377f82633eae26a46002471f
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2025-08-06 13:38:50 -0400

    Fix old test that broken when i moved the leaderboard away from .

--- src/page-tests/__tests__/TeamLeaderboard.test.tsx
+++ src/page-tests/__tests__/TeamLeaderboard.test.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import TeamLeaderboard from '../../pages/index';
-import { shallow } from 'enzyme'
-import toJson from 'enzyme-to-json'
+import React from "react";
+import TeamLeaderboard from "../../pages/TeamLeaderboardPage";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 
 describe("TeamLeaderboardPage", () => {
   test("TeamLeaderboardPage - should create snapshot", () => {
-    const wrapper = shallow(<TeamLeaderboard testMode={true}/>);
+    const wrapper = shallow(<TeamLeaderboard testMode={true} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
