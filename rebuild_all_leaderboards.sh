1492a03a3e6a7ac2f066a39f4552caac5ba5d7a2
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2025-09-09 17:17:43 -0400

    Allow indt men vs women leaderboard procesing

--- rebuild_all_leaderboards.sh
+++ rebuild_all_leaderboards.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
 # Use ENRICH=--enrich-rosters to update the rosters with positional info
 # Use EXTRA=--extra-data to run in NBA analysis mode (writes to ./enrichedPlayers)
+# Use GENDER_FILTER="men" || GENDER_FILTER="women" if you want only one gender
 if [ -z "$YEARS" ]; then
    echo "Specify YEARS=all|old|new|YYYY/YY, currently [$YEARS]"
    exit -1
 fi
 if [ "$YEARS" = "all" ] || [ "$YEARS" = "new" ]; then
-  npm run build_leaderboards -- --tier=High $ENRICH $EXTRA; npm run build_leaderboards -- --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --gender=Women $ENRICH $EXTRA; 
-   npm run build_leaderboards -- --tier=Combo $EXTRA
+   if [ "$GENDER_FILTER" != "women" ]; then
+      npm run build_leaderboards -- --tier=High $ENRICH $EXTRA; 
+      npm run build_leaderboards -- --tier=Medium $ENRICH $EXTRA; 
+      npm run build_leaderboards -- --tier=Low $ENRICH $EXTRA; 
+   fi
+   if [ "$GENDER_FILTER" != "men" ]; then
+      npm run build_leaderboards -- --gender=Women $ENRICH $EXTRA; 
+   fi
+   if [ "$GENDER_FILTER" != "women" ]; then
+      npm run build_leaderboards -- --tier=Combo $EXTRA
+   fi
 fi
 if [ "$YEARS" = "all" ] || [ "$YEARS" = "old" ]; then
-   npm run build_leaderboards -- --year=2023/24 --tier=High $ENRICH $EXTRA; npm run build_leaderboards -- --year=2023/24 --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --year=2023/24 --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --year=2023/24 --gender=Women --tier=High $ENRICH $EXTRA;
-   npm run build_leaderboards -- --tier=Combo --year=2023/24 $EXTRA
-   npm run build_leaderboards -- --year=2022/23 --tier=High $ENRICH $EXTRA; npm run build_leaderboards -- --year=2022/23 --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --year=2022/23 --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --year=2022/23 --gender=Women --tier=High $ENRICH $EXTRA;
-   npm run build_leaderboards -- --tier=Combo --year=2022/23 $EXTRA
-   npm run build_leaderboards -- --year=2021/22 --tier=High $ENRICH $EXTRA; npm run build_leaderboards -- --year=2021/22 --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --year=2021/22 --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --year=2021/22 --gender=Women --tier=High $ENRICH $EXTRA;
-   npm run build_leaderboards -- --tier=Combo --year=2021/22 $EXTRA
-   npm run build_leaderboards -- --year=2020/21 --tier=High $ENRICH $EXTRA; npm run build_leaderboards -- --year=2020/21 --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --year=2020/21 --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --year=2020/21 --gender=Women --tier=High $ENRICH $EXTRA;
-   npm run build_leaderboards -- --tier=Combo --year=2020/21 $EXTRA
+   for y in "2023/24" "2022/23" "2021/22" "2020/21"; do
+      if [ "$GENDER_FILTER" != "women" ]; then
+         npm run build_leaderboards -- --year=$y --tier=High $ENRICH $EXTRA; 
+         npm run build_leaderboards -- --year=$y --tier=Medium $ENRICH $EXTRA; 
+         npm run build_leaderboards -- --year=$y --tier=Low $ENRICH $EXTRA; 
+      fi
+      if [ "$GENDER_FILTER" != "men" ]; then
+         npm run build_leaderboards -- --year=$y --gender=Women --tier=High $ENRICH $EXTRA;
+      fi
+      if [ "$GENDER_FILTER" != "women" ]; then
+         npm run build_leaderboards -- --tier=Combo --year=$y $EXTRA
+      fi
+   done
    # Older data with only one tier:
-   npm run build_leaderboards -- --tier=High --year=2019/20 $ENRICH $EXTRA; npm run build_leaderboards -- --tier=High --year=2018/9 $ENRICH $EXTRA;
-   npm run build_leaderboards -- --gender=Women --year=2019/20 $ENRICH $EXTRA; npm run build_leaderboards -- --gender=Women --year=2018/9 $ENRICH $EXTRA; 
-   npm run build_leaderboards -- --tier=High --year=Extra $ENRICH $EXTRA; 
+   for y in "2019/20" "2018/9"; do
+      if [ "$GENDER_FILTER" != "women" ]; then
+         npm run build_leaderboards -- --tier=High --year=$y $ENRICH $EXTRA;
+      fi
+      if [ "$GENDER_FILTER" != "men" ]; then
+         npm run build_leaderboards -- --gender=Women --year=$y $ENRICH $EXTRA; 
+      fi
+      if [ "$GENDER_FILTER" != "women" ]; then
+         npm run build_leaderboards -- --tier=High --year=Extra $ENRICH $EXTRA; 
+      fi
+   done
 fi
 if [[ $YEARS =~ ^[0-9]{4}[/][0-9]{2}$ ]]; then
-   npm run build_leaderboards -- --year=$YEARS --tier=High $ENRICH $EXTRA;npm run build_leaderboards -- --year=$YEARS --tier=Medium $ENRICH $EXTRA; npm run build_leaderboards -- --year=$YEARS --tier=Low $ENRICH $EXTRA; npm run build_leaderboards -- --year=$YEARS --gender=Women --tier=High $ENRICH $EXTRA;
-   npm run build_leaderboards -- --tier=Combo --year=$YEARS $EXTRA
+   if [ "$GENDER_FILTER" != "women" ]; then
+      npm run build_leaderboards -- --year=$YEARS --tier=High $ENRICH $EXTRA;
+      npm run build_leaderboards -- --year=$YEARS --tier=Medium $ENRICH $EXTRA; 
+      npm run build_leaderboards -- --year=$YEARS --tier=Low $ENRICH $EXTRA; 
+   fi
+   if [ "$GENDER_FILTER" != "men" ]; then
+      npm run build_leaderboards -- --year=$YEARS --gender=Women --tier=High $ENRICH $EXTRA;
+   fi
+   if [ "$GENDER_FILTER" != "women" ]; then
+      npm run build_leaderboards -- --tier=Combo --year=$YEARS $EXTRA
+   fi
 fi
