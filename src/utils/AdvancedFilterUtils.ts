9fde8b93684c79c87197a1907711c1743d7e931d
Alex-At-Home <Alex-At-Home@users.noreply.github.com>
2026-02-27 18:59:33 -0500

    Improve half court / on-ball / off-ball derived style stats

--- src/utils/AdvancedFilterUtils.ts
+++ src/utils/AdvancedFilterUtils.ts
@@ -812,7 +812,7 @@ export class AdvancedFilterUtils {
   /** "Clever" replacements that happen after singleYearfixObjectFormat */
   static advancedSingleYearPreFieldReplacements(s: string) {
     // On-Ball
-    //off_style_rim_attack_pct + off_style_attack_kick_pct + off_style_dribble_jumper_pct + 0.5*off_style_mid_range_pct + off_style_hits_cutter_pct + off_style_hits_cutter_pct + off_style_pnr_passer_pct + off_style_post_up_pct + off_style_post_kick_pct + 0.5*off_style_high_low_pct
+    //off_style_rim_attack_pct + off_style_attack_kick_pct + off_style_dribble_jumper_pct + 0.5*off_style_mid_range_pct + off_style_hits_cutter_pct + off_style_pnr_passer_pct + off_style_post_up_pct + off_style_post_kick_pct + 0.5*off_style_high_low_pct
     // Off-Ball
     //off_style_perimeter_sniper_pct + 0.5*off_style_mid_range_pct + off_style_perimeter_cut_pct + off_style_big_cut_roll_pct + off_style_pick_pop_pct + 0.5*off_style_high_low_pct
 
@@ -838,15 +838,15 @@ export class AdvancedFilterUtils {
       ) //(these 3 will then get replaced as below)
       .replace(
         /off_style_onball_pct/g,
-        "(off_style_rim_attack_pct + off_style_attack_kick_pct + off_style_dribble_jumper_pct + 0.5*off_style_mid_range_pct + off_style_hits_cutter_pct + off_style_hits_cutter_pct + off_style_pnr_passer_pct + off_style_post_up_pct + off_style_post_kick_pct + 0.5*off_style_high_low_pct)/(off_style_total_halfcourt_pct)",
+        "(off_style_rim_attack_pct + off_style_attack_kick_pct + off_style_dribble_jumper_pct + 0.5*off_style_mid_range_pct + off_style_hits_cutter_pct + off_style_pnr_passer_pct + off_style_post_up_pct + off_style_post_kick_pct + 0.5*off_style_high_low_pct)/(off_style_total_halfcourt_pct)",
       )
       .replace(
         /off_style_onball_usg/g,
-        "(off_style_rim_attack_usg + off_style_attack_kick_usg + off_style_dribble_jumper_usg + 0.5*off_style_mid_range_usg + off_style_hits_cutter_usg + off_style_hits_cutter_usg + off_style_pnr_passer_usg + off_style_post_up_usg + off_style_post_kick_usg + 0.5*off_style_high_low_usg)",
+        "(off_style_rim_attack_usg + off_style_attack_kick_usg + off_style_dribble_jumper_usg + 0.5*off_style_mid_range_usg + off_style_hits_cutter_usg + off_style_pnr_passer_usg + off_style_post_up_usg + off_style_post_kick_usg + 0.5*off_style_high_low_usg)",
       )
       .replace(
         /off_style_onball_ppp/g,
-        "(off_style_rim_attack_pct*off_style_rim_attack_ppp + off_style_attack_kick_pct*off_style_attack_kick_ppp + off_style_dribble_jumper_pct*off_style_dribble_jumper_ppp + 0.5*off_style_mid_range_pct*off_style_mid_range_ppp + off_style_hits_cutter_pct*off_style_hits_cutter_ppp + off_style_hits_cutter_pct*off_style_hits_cutter_ppp + off_style_pnr_passer_pct*off_style_pnr_passer_ppp + off_style_post_up_pct*off_style_post_up_ppp + off_style_post_kick_pct*off_style_post_kick_ppp + 0.5*off_style_high_low_pct*off_style_high_low_ppp)/(off_style_rim_attack_pct + off_style_attack_kick_pct + off_style_dribble_jumper_pct + 0.5*off_style_mid_range_pct + off_style_hits_cutter_pct + off_style_hits_cutter_pct + off_style_pnr_passer_pct + off_style_post_up_pct + off_style_post_kick_pct + 0.5*off_style_high_low_pct)",
+        "(off_style_rim_attack_pct*off_style_rim_attack_ppp + off_style_attack_kick_pct*off_style_attack_kick_ppp + off_style_dribble_jumper_pct*off_style_dribble_jumper_ppp + 0.5*off_style_mid_range_pct*off_style_mid_range_ppp + off_style_hits_cutter_pct*off_style_hits_cutter_ppp + off_style_pnr_passer_pct*off_style_pnr_passer_ppp + off_style_post_up_pct*off_style_post_up_ppp + off_style_post_kick_pct*off_style_post_kick_ppp + 0.5*off_style_high_low_pct*off_style_high_low_ppp)/(off_style_rim_attack_pct + off_style_attack_kick_pct + off_style_dribble_jumper_pct + 0.5*off_style_mid_range_pct + off_style_hits_cutter_pct + off_style_pnr_passer_pct + off_style_post_up_pct + off_style_post_kick_pct + 0.5*off_style_high_low_pct)",
       )
       .replace(
         /off_style_offball_pct/g,
@@ -863,7 +863,7 @@ export class AdvancedFilterUtils {
       .replace(
         //(mainly used as a replacement val above)
         /off_style_total_halfcourt_pct/g,
-        "(off_style_rim_attack_pct + off_style_attack_kick_pct + off_style_dribble_jumper_pct + 0.5*off_style_mid_range_pct + off_style_hits_cutter_pct + off_style_hits_cutter_pct + off_style_pnr_passer_pct + off_style_post_up_pct + off_style_post_kick_pct + 0.5*off_style_high_low_pct)/(off_style_onball_pct + off_style_offball_pct + off_style_perimeter_sniper_pct + 0.5*off_style_mid_range_pct + off_style_perimeter_cut_pct + off_style_big_cut_roll_pct + off_style_pick_pop_pct + 0.5*off_style_high_low_pct)/(off_style_onball_pct + off_style_offball_pct)",
+        "(off_style_rim_attack_pct + off_style_attack_kick_pct + off_style_dribble_jumper_pct + 0.5*off_style_mid_range_pct + off_style_hits_cutter_pct + off_style_pnr_passer_pct + off_style_post_up_pct + off_style_post_kick_pct + 0.5*off_style_high_low_pct + off_style_perimeter_sniper_pct + 0.5*off_style_mid_range_pct + off_style_perimeter_cut_pct + off_style_big_cut_roll_pct + off_style_pick_pop_pct + 0.5*off_style_high_low_pct)",
       );
   }
 
